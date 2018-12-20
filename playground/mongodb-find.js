8352cda3a5c3a3c7df67c6bc332a5c395c0e9179
fordakias <alexanderdedos@gmail.com>
2018-12-20 12:54:35 +0200

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,32 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+  if (err) {
+    return console.log('Unable to connect MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5c1b5f6c9eabf31148b18797')
+  // }).toArray().then((docs)=>{
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) =>{
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Todos').find().count().then((count)=>{
+    console.log(`Todos count: ${count}`);;
+  }, (err) =>{
+    console.log('Unable to fetch todos', err);
+  });
+
+  db.collection('Users').find({name: 'Nikos'}).toArray().then((docs)=>{
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) =>{
+    console.log('Unable to fetch todos', err);
+  });
+
+  client.close();
+});
