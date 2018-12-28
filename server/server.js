7b806d5e04dde4964d25b4a1172c5a23669b94c5
fordakias <alexanderdedos@gmail.com>
2018-12-29 01:43:43 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -9,6 +10,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// POST GET _ Todos
 app.post('/todos',(req, res)=>{
   // console.log(req.body);
   var todo = new Todo ({
@@ -30,6 +32,26 @@ app.get('/todos',(req, res)=>{
   })
 });
 
+app.get('/todos/:id', (req, res) =>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    return res.status(404).send();
+  });
+});
+
+
+
+// POST GET _ User
 app.post('/user',(req, res)=>{
   // console.log(req.body);
   var user = new User ({
