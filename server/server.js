796dcf0c608fe52b2af81881b854b2a1802444ee
fordakias <alexanderdedos@gmail.com>
2019-01-04 11:22:48 +0200

    Logging In POST/users/Login Testing POST/users/login

--- server/server.js
+++ server/server.js
@@ -111,27 +111,24 @@ app.post('/users',(req, res)=>{
   });
 });
 
-
-
 app.get('/users/me',authenticate, (req, res) =>{
     res.send(req.user);
+});
+
+app.post('/users/login', (req,res) =>{
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email,body.password).then((user)=>{
+    return user.generateAuthToken().then((token)=>{
+    res.header('x-auth', token).send(user);
+    });
+  }).catch((e) =>{
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
-
-
-// var newTodo = new Todo({
-//   text: '  Cook dinner 2  '
-// });
-//
-// newTodo.save().then((doc)=> {
-//  console.log('Saved todo', doc);
-//  mongoose.connection.close();
-// }, (e) => {
-// console.log('Unable to save', e);
-//  mongoose.connection.close();
-// });
 
 module.exports = {app};
